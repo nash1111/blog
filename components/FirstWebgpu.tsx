44fdafbe9d5d0d939b9c9bc56d800776b34acc2a
nash1111 <ken1251213@gmail.com>
2023-05-30 01:10:51 +0900

    remove webgpu types

--- components/FirstWebgpu.tsx
+++ components/FirstWebgpu.tsx
@@ -1,8 +1,89 @@
+import React, { useState, useRef, useEffect } from "react";
 import triangleVertWGSL from "../shaders/triangle.vert.wgsl";
 import redFragWGSL from "../shaders/red.frag.wgsl";
 
-const FirstWebgpu = () => {
-  return <>FirstWebGPU Component</>;
+const HelloTriangle: React.FC = () => {
+  const [pageState, setPageState] = useState({ active: true });
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const init = async () => {
+      const adapter = await navigator.gpu.requestAdapter();
+      const device = await adapter.requestDevice();
+
+      if (!pageState.active) return;
+      const context = canvas.getContext("webgpu") as GPUCanvasContext;
+
+      const devicePixelRatio = window.devicePixelRatio || 1;
+      canvas.width = canvas.clientWidth * devicePixelRatio;
+      canvas.height = canvas.clientHeight * devicePixelRatio;
+      const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+
+      context.configure({
+        device,
+        format: presentationFormat,
+        alphaMode: "premultiplied",
+      });
+
+      const pipeline = device.createRenderPipeline({
+        layout: "auto",
+        vertex: {
+          module: device.createShaderModule({
+            code: triangleVertWGSL,
+          }),
+          entryPoint: "main",
+        },
+        fragment: {
+          module: device.createShaderModule({
+            code: redFragWGSL,
+          }),
+          entryPoint: "main",
+          targets: [
+            {
+              format: presentationFormat,
+            },
+          ],
+        },
+        primitive: {
+          topology: "triangle-list",
+        },
+      });
+
+      const frame = () => {
+        if (!pageState.active) return;
+
+        const commandEncoder = device.createCommandEncoder();
+        const textureView = context.getCurrentTexture().createView();
+
+        const renderPassDescriptor: GPURenderPassDescriptor = {
+          colorAttachments: [
+            {
+              view: textureView,
+              clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+              loadOp: "clear",
+              storeOp: "store",
+            },
+          ],
+        };
+
+        const passEncoder =
+          commandEncoder.beginRenderPass(renderPassDescriptor);
+        passEncoder.setPipeline(pipeline);
+        passEncoder.draw(3, 1, 0, 0);
+        passEncoder.end();
+
+        device.queue.submit([commandEncoder.finish()]);
+        requestAnimationFrame(frame);
+      };
+
+      requestAnimationFrame(frame);
+    };
+
+    init();
+  }, []);
+
+  return <canvas ref={canvasRef} />;
 };
 
-export default FirstWebgpu;
+export default HelloTriangle;
