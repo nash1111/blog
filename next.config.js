b0d158ffd2890c256e61faf90d34872327af0847
nash1111 <ken1251213@gmail.com>
2023-09-03 03:14:31 +0900

    feat: i hope it built

--- next.config.js
+++ next.config.js
@@ -37,6 +37,23 @@ const nextConfig = {
 
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
