847150738eb3c973dae9a148cbedc4170dd92f12
nash1111 <nash1111@users.noreply.github.com>
2023-12-29 23:25:57 +0900

    refactor: with type error

--- components/HelloTriangle.tsx
+++ components/HelloTriangle.tsx
@@ -3,95 +3,98 @@ import triangleVertWGSL from "../shaders/triangle.vert.wgsl";
 import redFragWGSL from "../shaders/red.frag.wgsl";
 
 const HelloTriangle: React.FC = () => {
-  const [pageState, setPageState] = useState({ active: true });
+  const [isActive, setIsActive] = useState(true);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const init = async () => {
-      const adapter = await navigator.gpu?.requestAdapter();
-      const device = await adapter?.requestDevice();
+    if (!isActive) return;
 
-      if (!pageState.active) return;
-      if (!canvas) {
-        console.error("canvas not defined");
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      console.error("Canvas not defined");
+      return;
+    }
+
+    const initWebGPU = async () => {
+      const adapter = await navigator.gpu.requestAdapter();
+      if (!adapter) {
+        console.error("WebGPU adapter is not available");
         return;
       }
+
+      const device = await adapter.requestDevice();
+      if (!device) {
+        console.error("WebGPU device is not available");
+        return;
+      }
+
       const context = canvas.getContext("webgpu") as GPUCanvasContext;
+      const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
 
       const devicePixelRatio = window.devicePixelRatio || 1;
       canvas.width = canvas.clientWidth * devicePixelRatio;
       canvas.height = canvas.clientHeight * devicePixelRatio;
-      const presentationFormat = navigator.gpu?.getPreferredCanvasFormat();
 
-      if (!device) {
-        console.error("Device is not available");
-        return;
-      }
       context.configure({
-        device: device as any,
-        format: presentationFormat as GPUTextureFormat,
+        device,
+        format: presentationFormat,
         alphaMode: "premultiplied",
       });
 
-      const pipeline = device.createRenderPipeline({
-        layout: "auto",
-        vertex: {
-          module: device.createShaderModule({
-            code: triangleVertWGSL,
-          }) as GPUShaderModule,
-          entryPoint: "main",
-        },
-        fragment: {
-          module: device.createShaderModule({
-            code: redFragWGSL,
-          }) as GPUShaderModule,
-          entryPoint: "main",
-          targets: [
-            {
-              format: presentationFormat,
-            },
-          ] as any,
-        },
-        primitive: {
-          topology: "triangle-list",
-        },
-      });
+      const pipeline = createRenderPipeline(device, presentationFormat);
 
-      const frame = () => {
-        if (!pageState.active) return;
+      const render = () => {
+        if (!isActive) return;
 
         const commandEncoder = device.createCommandEncoder();
         const textureView = context.getCurrentTexture().createView();
+        const renderPassDescriptor = createRenderPassDescriptor(textureView);
 
-        const renderPassDescriptor: GPURenderPassDescriptor = {
-          colorAttachments: [
-            {
-              view: textureView,
-              clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
-              loadOp: "clear" as const,
-              storeOp: "store" as const,
-            },
-          ],
-        };
-
-        const passEncoder =
-          commandEncoder.beginRenderPass(renderPassDescriptor);
+        const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
         passEncoder.setPipeline(pipeline);
         passEncoder.draw(3, 1, 0, 0);
         passEncoder.end();
 
         device.queue.submit([commandEncoder.finish()]);
-        requestAnimationFrame(frame);
+        requestAnimationFrame(render);
       };
 
-      requestAnimationFrame(frame);
+      requestAnimationFrame(render);
     };
 
-    init();
-  }, [pageState.active]);
+    initWebGPU();
+  }, [isActive]);
 
   return <canvas ref={canvasRef} />;
 };
+
+function createRenderPipeline(device: GPUDevice, format: GPUTextureFormat): GPURenderPipeline {
+  return device.createRenderPipeline({
+    layout: "auto",
+    vertex: {
+      module: device.createShaderModule({ code: triangleVertWGSL }),
+      entryPoint: "main",
+    },
+    fragment: {
+      module: device.createShaderModule({ code: redFragWGSL }),
+      entryPoint: "main",
+      targets: [{ format }],
+    },
+    primitive: { topology: "triangle-list" },
+  });
+}
+
+function createRenderPassDescriptor(textureView: GPUTextureView): GPURenderPassDescriptor {
+  return {
+    colorAttachments: [
+      {
+        view: textureView,
+        clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+        loadOp: "clear",
+        storeOp: "store",
+      },
+    ],
+  };
+}
 
 export default HelloTriangle;
